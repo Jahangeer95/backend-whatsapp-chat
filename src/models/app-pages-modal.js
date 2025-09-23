4fcd6698f896474b002c77ce00524e80ed59a360
Jahangeer95 <alijahangir95@gmail.com>
2025-09-23 11:12:50 +0500

    method added for save to handle duplication error.

--- src/models/app-pages-modal.js
+++ src/models/app-pages-modal.js
@@ -22,4 +22,21 @@ const appPagesSchema = new Schema(
   { versionKey: false }
 );
 
+appPagesSchema.post("save", function (error, doc, next) {
+  if (error.name === "MongoServerError" && error.code === 11000) {
+    console.log(error.name, error.code);
+    const field = Object.keys(error.keyValue)[0]; // e.g., 'username' or 'email'
+    const value = error.keyValue[field];
+
+    const validationError = new Error(
+      `The ${field} '${value}' is already taken. Please choose a different one.`
+    );
+    validationError.statusCode = 409;
+
+    next(validationError);
+  } else {
+    next(error);
+  }
+});
+
 exports.AppFbPages = model("AppFbPages", appPagesSchema);
