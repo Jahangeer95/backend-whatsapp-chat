067c4583e1db93b8cffc295aac85f8ea1d57b333
Jahangeer95 <alijahangir95@gmail.com>
2025-10-03 09:40:41 +0500

    patch method added in cors.

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.set("io", io); // OR export io
 
 const corsOptions = {
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   exposedHeaders: ["user_auth_token"],
   // exposedHeaders: [],
   // allowedHeaders: [],
